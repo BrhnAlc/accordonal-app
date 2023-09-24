eef8083b7aec7a736304e5ae0b723f1e579e729e
burhan <alacaburhan122016@gmail.com>
2023-09-25 01:33:12 +0300

    accordion

---
+++ src/Accordion.jsx
@@ -0,0 +1,19 @@
+
+
+const Accordion = () => {
+  return (
+    <div className="accordion" >  
+    <div className="accordionHeading" >
+    <div className="container">
+    <p>Question 1</p>
+    <span>Click Me</span>
+    </div>
+    </div>
+    <div>
+        <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+    </div>
+  )
+}
+
+export default Accordion
--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
+import { useState } from 'react';
+import Accordion from './Accordion';
 import './App.css';
 
 function App() {
+  const [acor, setAcor] = useState(0)
   return (
     <div className="App">
-      
+      <h1>FAQ APP</h1>
+      <Accordion/>
     </div>
   );
 }
